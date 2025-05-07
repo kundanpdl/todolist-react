6d4c7d3be5b7a5611386ecf9f4efa8f7762ea1a6
Kundan Paudel <paudelkundan@gmail.com>
2025-05-07 15:24:00 -0600

    Header added along with some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import Todo from "./components/Todo";
+import Header from "./components/Header";
+import "./App.css";
 function App() {
   return (
     <div className="App">
+      <Header />
       <Todo />
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import styles from "./header.module.css";
+
+const Header = () => {
+  return <h1 className={styles.header}> My Todo List</h1>;
+};
+
+export default Header;
