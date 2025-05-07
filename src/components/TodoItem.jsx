b660aa6a00651d044050181e2b8b89ff37f2580a
Kundan Paudel <paudelkundan@gmail.com>
2025-05-07 16:35:34 -0600

    delete functionality added, UI fixed for todo items list and the item itself

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,8 +1,22 @@
 import styles from "./todoitem.module.css";
-export default function TodoItem({ item }) {
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    setTodos(todos.filter((todo) => todo !== item)); // Returns all items that do not match the condition and does not return the one that matches
+  }
   return (
     <div className={styles.item}>
-      <div className={styles.itemName}>{item}</div>
+      <div className={styles.itemName}>
+        {item}
+        <span>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
+            x
+          </button>
+        </span>
+      </div>
+
       <hr className={styles.divider} />
     </div>
   );
