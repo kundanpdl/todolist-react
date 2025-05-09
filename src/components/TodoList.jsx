b9d9aab90466bf812b802afbf0599454d90ade2c
Kundan Paudel <181020254+kundanpdl@users.noreply.github.com>
2025-05-09 11:39:22 -0600

    Completed Project, added sorting functionality

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,18 @@
 import TodoItem from "./TodoItem";
 import styles from "./todolist.module.css";
 export default function TodoList({ todos, setTodos }) {
+  const sortedTodos = todos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done)); // Slice creates copy of the array and Number converts boolean to number
   return (
     <div className={styles.list}>
-      {todos.map((item) => (
-        <TodoItem key={item.name} item={item} todos={todos} setTodos={setTodos} />
+      {sortedTodos.map((item) => (
+        <TodoItem
+          key={item.name}
+          item={item}
+          todos={todos}
+          setTodos={setTodos}
+        />
       ))}
     </div>
   );
